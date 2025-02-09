acd85c42465052931c57ee06b515e76ab017856c
Nchavelelo <NchaveleloSithole@gmail.com>
2025-02-09 19:10:05 +0200

    Update File Structure

---
+++ src/pages/ProfileUpdate/profileUpdate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './profileUpdate.css'
+
+const profileUpdate = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default profileUpdate
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import './login.css';
 
-const App = () => {
+const login = () => {
   return (
     <div>
       
@@ -8,4 +9,4 @@ const App = () => {
   )
 }
 
-export default App
+export default login
