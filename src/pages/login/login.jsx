99d08ddbc7c86c9a0b04dd409048046bd5d6eb5a
Nchavelelo <NchaveleloSithole@gmail.com>
2025-02-11 01:54:10 +0200

    Style Sign Up form

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -8,16 +8,16 @@ const login = () => {
       <img src={assets.logo_big} alt="Chat Logo" className="logo" />
       <form  className="login-form">
       <h2>Sign Up</h2>
-        <input type="text" placeholder='username' className="form-input" required />
-        <input type="email" placeholder='Email Address' className="form-input" />
-        <input type="password" placeholder='password' className="form-input" />
+        <input type="text" placeholder='Username' className="form-input" required />
+        <input type="email" placeholder='Email Address' className="form-input" required />
+        <input type="password" placeholder='Password' className="form-input" required/>
         <button type='submit' >Sign Up</button>
         <div className="login-term">
             <input type="checkbox" />
             <p>Agree to the terms of use & privacy policy.</p>
         </div>
         <div className="login-forgot">
-            <p className="login-toggle">Already have an account <span></span>click here</p>
+            <p className="login-toggle">Already have an account <span>click here</span></p>
         </div>
 
       </form>
